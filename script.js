1e39bc621b4779277f09b851ab6aa46b3b1c2dca
Chowdhury Tasbih Ahmed Siddique <ctasbihas@gmail.com>
2022-12-28 15:48:57 +0600

    replace server link

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://chatai-server-cf63.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
